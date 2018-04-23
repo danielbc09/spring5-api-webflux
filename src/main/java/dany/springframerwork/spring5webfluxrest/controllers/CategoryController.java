5e6d50d449bdb9d2f9af567131829a4e385a66aa
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-04-23 12:06:03 -0500

    Created A new Category

--- src/main/java/dany/springframerwork/spring5webfluxrest/controllers/CategoryController.java
+++ src/main/java/dany/springframerwork/spring5webfluxrest/controllers/CategoryController.java
@@ -2,9 +2,9 @@ package dany.springframerwork.spring5webfluxrest.controllers;
 
 import dany.springframerwork.spring5webfluxrest.domain.Category;
 import dany.springframerwork.spring5webfluxrest.repositories.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -23,5 +23,11 @@ public class CategoryController {
     @GetMapping("/api/v1/categories/{id}")
     Mono<Category> getById(@PathVariable String id){
         return categoryRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/categories")
+    Mono<Void> create(@RequestBody Publisher <Category> categoryStream) {
+        return categoryRepository.saveAll(categoryStream).then();
     }
 }
